d30ad4df04a76d6cdccba3d7ae015f232be81f3c
Sogol Nezami <sogol.nezami@check24.de>
2026-05-01 20:30:07 +0200

    Add login credentials and user info fixtures; handle uncaught exceptions in tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,5 @@
 import './commands'
 
+// Demoblaze throws uncaught JS errors unrelated to the test flow (e.g. third-party scripts).
+// Returning false prevents Cypress from failing the test for those.
+Cypress.on('uncaught:exception', () => false)
